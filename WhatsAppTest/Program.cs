41ecbce6e640a50e863fd9038ca489757d16c94c
Xien Singh <xiensingh007@gmail.com>
2018-08-24 10:57:12 +0200

    Removed contact details

--- WhatsAppTest/Program.cs
+++ WhatsAppTest/Program.cs
@@ -10,28 +10,23 @@ namespace WhatsAppTest
     {
         static void Main(string[] args)
         {
-            // Find your Account Sid and Token at twilio.com/console
-            const string accountSid = "ACa59e536d3a7129f3d0da33a8fd0eae4d";
-            const string authToken = "4a78df272cd6ae44ea4856f74d8ff2a3";
+            const string accountSid = "##";//Provider Provided
+            const string authToken = "##";//Provider Provided
 
             TwilioClient.Init(accountSid, authToken);
-            //0716818319
-            var numbers = new List<string>
-            {
-                "+27716818319",
-                "+27837074655"
-            };
-            Dictionary<string, string> openWith = new Dictionary<string, string>();
 
-            openWith.Add("Adrian", "+27716818319");
-            openWith.Add("Xien", "+27837074655");
+            //OverKill for now until a DB is created, and a premium  Acc
+            Dictionary<string, string> Contacts = new Dictionary<string, string>();
+
+            Contacts.Add("Person1", "+#####");
+            Contacts.Add("Person2", "+#####");
 
 
-            foreach (var x in openWith)
+            foreach (var x in Contacts)
             {
                 var message = MessageResource.Create(
-                body: "This is an iX Test, Have a look at this TEST "+ x.Key,
-                from: new PhoneNumber("whatsapp:+14155238886"),
+                body: "This is a Test, Have a look at this TEST : "+ x.Key,
+                from: new PhoneNumber("whatsapp:+###"), //Provider ID
                 to: new PhoneNumber("whatsapp:"+x.Value)
             );
                 Console.WriteLine(message.Sid);
